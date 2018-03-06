c85c1707e6a889b7dc86dc9521b68dab5f920539
Ted Newton <32013641+newtdogg@users.noreply.github.com>
2018-03-06 20:30:07 +0000

    made the list of items a component, currently have put the inventory array in the vue code as I have trouble when accessing from elsewhere, will attempt to fix this later on

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,29 @@
   </div>
 </template>
 
-<script src="./shoppingCart.js"></script>
+<script src="./data.js"></script>
 
 <script>
+
+  Vue.filter( "subvalue", function( val ) {
+        return _.multiply( val.price, val.quantity );
+      });
+      Vue.filter( "tocurrency", function( num ) {
+        return accounting.formatMoney( num );
+      });
+      Vue.filter( "zeropad", function( num ) {
+        if ( num !== 0 ) {
+          return _.padStart( num, 2, '0' );
+        } else {
+          return 0;
+        }
+      });
+      Vue.filter( "titlecase", function( str ) {
+        return _.startCase( str );
+      });
+      Vue.filter( "textcapitalized", function( str ) {
+        return _.capitalize( str );
+      });
   // import searchBar from './components/searchBar.vue'
   import listOfItems from './components/listOfItems.vue'
   export default{
--- src/components/listOfItems.vue
+++ src/components/listOfItems.vue
@@ -12,30 +12,70 @@
 </template>
 
 <script>
-
+import inventory from '../data.js'
 	export default {
 	name: 'listOfItems',
 	  data() {
 	  	return{
           products: [{
-		    'name': 'USB Stick',
+    'name': 'USB Stick',
 
-		    'options': [{
-		        'id': 1,
-		        'details': '16GB',
-		        'price': 6.95
-		    }, {
-		        'id': 2,
-		        'details': '32GB',
-		        'price': 12.95
-		    }, {
-		        'id': 3,
-		        'details': '64GB',
-		        'price': 25.95
-		    }]
-		  }]
+    'options': [{
+        'id': 1,
+        'details': '16GB',
+        'price': 6.95
+    }, {
+        'id': 2,
+        'details': '32GB',
+        'price': 12.95
+    }, {
+        'id': 3,
+        'details': '64GB',
+        'price': 25.95
+    }]
+  }, {
+    'name': 'USB Plug',
+
+    'options': [{
+        'id': 4,
+        'price': 4.35,
+        'details': '3ft cable'
+    }, {
+        'id': 13,
+        'price': 9,
+        'details': '6ft cable'
+    }]
+  }, {
+    'name': 'Small Phone',
+
+    'options': [{
+        'id': 5,
+        'price': 199,
+        'details': 'Nokia Phone'
+    }]
+  }, {
+    'name': 'Camera',
+
+    'options': [{
+        'id': 6,
+        'price': 76.50,
+        'details': 'Blue Camera'
+    }, {
+        'id': 7,
+        'price': 86.50,
+        'details': 'Red Camera'
+    }, {
+        'id': 8,
+        'price': 55.50,
+        'details': 'Yellow Camera'
+    }, {
+        'id': 9,
+        'price': 45.50,
+        'details': 'Purple Camera'
+    }]
+  }]
         }
-    },
+   	 },
 	  	methods: {
 	    reverseMessage: function () {
 	      this.message = this.message.split('').reverse().join('')
